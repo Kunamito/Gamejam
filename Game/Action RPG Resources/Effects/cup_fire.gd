337ebf261cddf3d92223cb4bd7b16ad5ce6c2605
Ethan Gillaux-Genelle <ethan.gillaux-genelle@epitech.eu>
2024-05-19 22:23:47 +0200

    [ADD]: new map and win condition

--- Game/Action RPG Resources/Effects/cup_fire.gd
+++ Game/Action RPG Resources/Effects/cup_fire.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
+onready var text = $Finish
+
+var points = 0
 
 func _on_body_entered(body):
 	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	points += 1
+	if points == 2:
+		print("Go get the final one !\n")
+	queue_free()
+	pass
--- Game/points.gd
+++ Game/points.gd
@@ -1,9 +1,1 @@
-extends Node
 
-onready var text = $Label
-
-var points = 0
-
-func add_points():
-	points += 1
-	text.text = "Points " + str(points)
